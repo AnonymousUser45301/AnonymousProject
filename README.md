471d1cccbcc27687bc9e671489f137b1693c7619
AnonymousUser45301 <165129851+AnonymousUser45301@users.noreply.github.com>
2024-03-29 14:12:30 +0800

    Upload files

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# ICML2024_Stability_Generalization_FreeAT
+# Anonymous Porject
+
+This repository provides an official implementation of a paper that under double-blind review. The repository will be de-anonymized and released to the public after the review process.
+
+
+## Getting started
+
+To adversarially train a ResNet-18 model against $L_2$-norm attack of radius 128.0/255.0 by the free-AT algorithm, run: 
+`python train.py   --data cifar10  --method free  --attack L2  --eps 128.0  --model res18  --save_path cifar10_l2_free`
+
+To evaluate the model against [AutoAttack](https://github.com/fra31/auto-attack), run: 
+`python3 test_autoattack.py  --model_path cifar10_l2_free  --data cifar10  --attack L2  --eps 128.0  --seed 1 `
+
+
+
+## Performance Comparison
+You can download the pretrained models in this [anonymous Google drive link](https://drive.google.com/drive/folders/18so78-vONvd6lZqLs26IwnaAXxfN1BvR?usp=drive_link). The outputs are saved in `model_pth/MODEL_NAME/test_autoattack_output.log`, and it can be reproduced bt setting random seed as 1. 
+
+### Robust accuracy of ResNet-18 models against AutoAttack
+
+| CIFAR-10, $L_2$-norm Attack | Vanilla | Fast | Free | 
+| ------ | ------ | ----- | ---- | 
+| Train Accuracy (%) | 100.0 | 87.1 | 87.0 | 
+| Test Accuracy (%) | 64.9 | 57.5 | 64.9 | 
+| Generalization Gap (%) | 35.1 | 29.6 | 22.1 | 
+
+
+| CIFAR-10, $L_\infty$-norm Attack | Vanilla | Fast | Free | 
+| ------ | ------ | ----- | ---- | 
+| Train Accuracy (%) | 89.1 | 54.4 | 57.4 | 
+| Test Accuracy (%) | 40.7 | 30.0 | 42.3 | 
+| Generalization Gap (%) | 48.4 | 23.4 | 15.1 | 
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ test_autoattack.py
@@ -0,0 +1,109 @@
+import sys
+import os
+import argparse
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.autograd import Variable
+import torchvision.datasets as dset
+import torchvision.transforms as transforms
+from torchvision.utils import save_image
+from torch.utils.data import DataLoader
+
+import shutil
+import setproctitle
+import numpy as np
+import random
+
+import models, wideresnet
+from autoattack import AutoAttack
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--batchSz', type=int, default=250)
+    parser.add_argument('--data', type=str, default='cifar10', choices=('cifar10', 'cifar100'))
+    parser.add_argument('--num_samples', type=int, default=10000)
+
+    parser.add_argument('--attack', type=str, default='L2', choices=('Linf', 'L2'))
+    parser.add_argument('--eps', type=float, default=128.0)
+    parser.add_argument('--model_path', type=str)
+
+    parser.add_argument('--seed', type=int, default=1)
+
+    args = parser.parse_args()
+
+    args.eps = args.eps / 255
+
+
+    torch.manual_seed(args.seed)
+    torch.cuda.manual_seed(args.seed)
+    np.random.seed(args.seed)
+    random.seed(args.seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    os.environ["PYTHONHASHSEED"] = str(args.seed)
+
+
+    orig_stdout = sys.stdout
+    f = open(os.path.join('model_pth', args.model_path, 'test_autoattack_output.log'), 'w')
+    #sys.stdout = f
+
+
+    transform = transforms.Compose([transforms.ToTensor()])
+    if args.data == 'cifar10':
+        trainDataset = dset.CIFAR10(root='data/cifar10', train=True, download=True, transform=transform)
+        testDataset = dset.CIFAR10(root='data/cifar10', train=False, download=True, transform=transform)
+    elif args.data == 'cifar100':
+        trainDataset = dset.CIFAR100(root='data/cifar100', train=True, download=True, transform=transform)
+        testDataset = dset.CIFAR100(root='data/cifar100', train=False, download=True, transform=transform)
+
+    kwargs = {'num_workers': 0} 
+    trainLoader = DataLoader(trainDataset, batch_size=args.batchSz, shuffle=False, **kwargs)
+    testLoader = DataLoader(testDataset, batch_size=args.batchSz, shuffle=False, **kwargs)
+   
+
+    net = torch.load(os.path.join('model_pth', args.model_path, 'latest.pth'), map_location=torch.device('cuda'))
+
+    adversary = AutoAttack(net, norm=args.attack, eps=args.eps, seed=args.seed,  version='standard')
+
+    print('\n\n')
+    print('*'*40)
+    print('Train error against auto attack: ')
+    print('*'*40)
+    aa_test(args, net, trainLoader, adversary)
+
+    print('\n\n')
+    print('*'*40)
+    print('Test error against auto attack: ')
+    print('*'*40)
+    aa_test(args, net, testLoader, adversary)
+
+    sys.stdout = orig_stdout
+    f.close()
+
+
+
+def aa_test(args, net, testLoader, attacker):
+    net.eval()
+    image = None
+    labels = None
+    
+    for data, target in testLoader:
+        data, target = data.cuda(), target.cuda()
+
+        if image == None:
+            image = data
+            labels = target
+        else:
+            image = torch.cat((image, data), 0)
+            labels = torch.cat((labels, target), 0)
+
+    attacker.run_standard_evaluation(image[:args.num_samples], labels[:args.num_samples], bs=args.batchSz)
+
+
+
+
+if __name__=='__main__':
+    main()
